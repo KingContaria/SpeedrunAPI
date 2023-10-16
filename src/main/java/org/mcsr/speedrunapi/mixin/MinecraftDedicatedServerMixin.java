d04e03414a08233ef7249cefce7200a97ce04e28
contaria <kingcontaria@gmail.com>
2023-10-16 17:04:29 +0200

    add PostLaunchEntryPoint on the dedicated server

---
+++ src/main/java/org/mcsr/speedrunapi/mixin/MinecraftDedicatedServerMixin.java
@@ -0,0 +1,17 @@
+package org.mcsr.speedrunapi.mixin;
+
+import net.minecraft.server.dedicated.MinecraftDedicatedServer;
+import org.mcsr.speedrunapi.config.SpeedrunConfigAPI;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(MinecraftDedicatedServer.class)
+public abstract class MinecraftDedicatedServerMixin {
+
+    @Inject(method = "setupServer", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/dedicated/MinecraftDedicatedServer;loadWorld()V"))
+    private void onPostLaunchEntrypoint(CallbackInfoReturnable<Boolean> cir) {
+        SpeedrunConfigAPI.onPostLaunch();
+    }
+}
