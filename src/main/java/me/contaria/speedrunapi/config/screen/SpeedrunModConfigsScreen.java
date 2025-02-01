46c24df0c198e9bcee7a8e7a3cc2a41508e3744b
contaria <kingcontaria@gmail.com>
2025-02-01 13:01:45 +0100

    fix: draw title after list widgets

--- src/main/java/me/contaria/speedrunapi/config/screen/SpeedrunModConfigsScreen.java
+++ src/main/java/me/contaria/speedrunapi/config/screen/SpeedrunModConfigsScreen.java
@@ -22,8 +22,8 @@ public class SpeedrunModConfigsScreen extends Screen {
     @Override
     public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
         this.renderBackground(matrices);
-        DrawableHelper.drawCenteredTextWithShadow(matrices, this.textRenderer, this.title, this.width / 2, 10, 0xFFFFFF);
         super.render(matrices, mouseX, mouseY, delta);
+        DrawableHelper.drawCenteredTextWithShadow(matrices, this.textRenderer, this.title, this.width / 2, 10, 0xFFFFFF);
     }
 
     @Override
