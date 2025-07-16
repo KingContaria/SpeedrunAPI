5cac427791fcacf4f0f00310862bb0b6dc301f9c
tildejustin <spacepepper3.14@gmail.com>
2025-07-16 21:10:54 +0200

    feature: onConfigScreenClose api method (#22)

--- src/main/java/me/contaria/speedrunapi/config/screen/SpeedrunConfigScreen.java
+++ src/main/java/me/contaria/speedrunapi/config/screen/SpeedrunConfigScreen.java
@@ -97,6 +97,7 @@ public class SpeedrunConfigScreen extends Screen {
     public void close() {
         assert this.client != null;
         this.client.setScreen(this.parent);
+        this.config.getConfig().onConfigScreenClose(this, this.parent);
     }
 
     @Override
